5430e2daa7225fef077706a50b7636f51bca6cca
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-28 19:17:48 +0400

    Styling React Application

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,26 +49,35 @@ const pizzaData = [
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
-    </>
+    </div>
   );
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  // const style = {
+  //   color: "red",
+  //   fontSize: "48px",
+  // };
+
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
-    </>
+    </main>
   );
 }
 
@@ -80,14 +90,14 @@ function Footer() {
   // if (hour >= openHour && hour <= closeHour) alert("we are current;y open");
   // else alert("NOT open");
 
-  return <footer>{isOpn} we are open</footer>;
+  return <footer className="footer">{isOpn} we are open</footer>;
 }
 
 function Pizza() {
   return (
     <>
       <img src="/pizzas/spinaci.jpg" alt="why" />
-      <h2>Pizza</h2>
+      <h3>Pizza</h3>
       <p>text</p>
     </>
   );
