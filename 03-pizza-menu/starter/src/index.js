a1aa3f361be69a09f44f5bd1c36a2afe7df2068d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-30 12:55:10 +0400

    Setting Classes and Text Conditionally

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -119,7 +119,7 @@ function Pizza({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={`${pizzaObj.photoName}`} alt={pizzaObj.name} />
 
       <div>
