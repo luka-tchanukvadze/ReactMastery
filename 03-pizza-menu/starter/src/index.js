d7875b06fe1f3cc0e66c33c2b16d79707d0cf676
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-30 12:46:23 +0400

    Destructuring Props

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -112,16 +112,16 @@ function Order() {
   return <p>Open and order</p>;
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
-      <img src={`${props.pizzaObj.photoName}`} alt={props.pizzaObj.name} />
+      <img src={`${pizzaObj.photoName}`} alt={pizzaObj.name} />
 
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredient}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredient}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
