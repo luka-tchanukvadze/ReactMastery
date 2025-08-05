f02d352047ec2f8a2e977ba6105fd2bc0d7cd2c5
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-05 18:29:40 +0400

    init

--- 05-travel-list/starter/src/App.jsx
+++ 05-travel-list/starter/src/App.jsx
@@ -1,12 +1,34 @@
 export default function App() {
   return (
     <>
-      <div> hey</div>
+      <div className="app">
+        <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+      </div>
     </>
   );
 }
 
-function Logo() {}
-function Form() {}
-function PackingList() {}
-function Stats() {}
+function Logo() {
+  return <h1>😁 Fara Away</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-from">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      You have X items on your list, and you already packed x (x%)
+    </footer>
+  );
+}
