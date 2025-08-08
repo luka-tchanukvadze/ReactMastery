4b17819c696629a428b90686e4ffcc7f7a112e40
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-08 16:34:22 +0400

    The children Prop Making a Reusable Button

--- 04-steps/starter/src/App-v1.jsx
+++ 04-steps/starter/src/App-v1.jsx
@@ -6,7 +6,7 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-export default function App() {
+function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -36,21 +36,18 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor={"#7950f2"}
-              textColor={"white"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
               onClick={handlePrevious}
             >
-              <span>👌</span> Previous
-            </Button>
-
-            <Button
-              bgColor={"#7950f2"}
-              textColor={"white"}
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
               onClick={handleNext}
             >
-              Next <span>👌</span>
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
@@ -58,13 +55,4 @@ export default function App() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-}
+export default App;
