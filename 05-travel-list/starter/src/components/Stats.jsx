72ec59a25b731409d6c630aec8033ad2c71be1fa
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-08 15:05:49 +0400

    Moving Components Into Separate Files

---
+++ 05-travel-list/starter/src/components/Stats.jsx
@@ -0,0 +1,19 @@
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list</p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      You have {numItems} items on your list, and you already packed {numPacked}
+      ({percentage}%)
+    </footer>
+  );
+}
+
+export default Stats;
