a69a90dc6b02715a5311d61909b14bd850e9442b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-09 17:28:20 +0400

    Building the Static App Forms

--- 06-eat-n-split/learning/src/App.jsx
+++ 06-eat-n-split/learning/src/App.jsx
@@ -25,8 +25,13 @@ export default function App() {
       <div className="app">
         <div className="sidebar">
           <FriendsList />
+
           <FormAddFriend />
+
+          <Button>Add Friend</Button>
         </div>
+
+        <FormSplitBill />
       </div>
     </>
   );
@@ -80,6 +85,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>Bill value</label>
+      <input type="text" />
+
+      <label>Your expense</label>
+      <input type="text" />
+
+      <label>X's expense</label>
+      <input type="text" disabled />
+
+      <label>Who is paying the bill</label>
+      <select>
+        <option value={"user"}>You</option>
+        <option value={"friend"}>X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
