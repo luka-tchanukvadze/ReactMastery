d6da6ca5b97e6419b0eaa5411441487b14ec2d55
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-11 18:01:54 +0400

    Handling Hover Events

--- 07-usepopcorn/learning/src/components/StarRating.jsx
+++ 07-usepopcorn/learning/src/components/StarRating.jsx
@@ -18,6 +18,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   return (
     <>
@@ -27,11 +28,13 @@ export default function StarRating({ maxRating = 5 }) {
             <Star
               key={i}
               onClick={() => setRating(i + 1)}
-              full={rating >= i + 1}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
             />
           ))}
         </div>
-        <p style={textStyle}>{rating || ""}</p>
+        <p style={textStyle}>{tempRating || rating || ""}</p>
       </div>
     </>
   );
@@ -44,9 +47,15 @@ const starStyle = {
   cusrsor: "pointer",
 };
 
-function Star({ onClick, full }) {
+function Star({ onClick, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onClick}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onClick}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
