97b5c9de5d07f3535d79bb99c8df9cdbc225a1a6
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-28 18:24:55 +0400

    Loading Movie Details

--- 07-usepopcorn/learning/src/App.jsx
+++ 07-usepopcorn/learning/src/App.jsx
@@ -322,11 +322,52 @@ function WatchedBox() {
 */
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: gentre,
+  } = movie;
+
+  console.log(title, year);
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      setMovie(data);
+    }
+
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+
+        <img src={poster} alt="poster" />
+
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+        </div>
+      </header>
+
       {selectedId}
     </div>
   );
