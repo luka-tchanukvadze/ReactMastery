780acab37dcf735dac08fd3995522c88931b8502
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-08-31 18:17:09 +0400

    fixing custom hook

--- 07-usepopcorn/learning/src/App.jsx
+++ 07-usepopcorn/learning/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
-  const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
+  const { movies, isLoading, error } = useMovies(query);
 
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
