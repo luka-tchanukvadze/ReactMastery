56ae9d52ede6a670c868f2c5e20d5da61ef1c6c3
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-03 15:20:43 +0400

    fetching data

--- 10-react-quiz/learning/src/App.jsx
+++ 10-react-quiz/learning/src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import MainSec from "./MainSec";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
+
   return (
     <>
       <div className="app">
