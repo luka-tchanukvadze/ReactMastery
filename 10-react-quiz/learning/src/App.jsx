8f0303ccdf351ec8ff27c136aa9014d435e1e09b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-04 18:11:33 +0400

    Handling New Answers

--- 10-react-quiz/learning/src/App.jsx
+++ 10-react-quiz/learning/src/App.jsx
@@ -14,6 +14,7 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -34,13 +35,19 @@ function reducer(state, action) {
     case "start":
       return { ...state, status: "active" };
 
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
+
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -65,7 +72,13 @@ function App() {
           {status === "ready" && (
             <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
           )}
-          {status === "active" && <Question question={questions[index]} />}
+          {status === "active" && (
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+          )}
         </MainSec>
       </div>
     </>
