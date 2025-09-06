c00332fdfc1d00424838c140f30a2e79985d15a2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-06 23:17:02 +0400

    Building the App Layout

--- 11-worldwise/learning/src/pages/AppLayout.jsx
+++ 11-worldwise/learning/src/pages/AppLayout.jsx
@@ -1,15 +1,16 @@
-// import Map from "../components/Map";
+import Map from "../components/Map";
 // import Sidebar from "../components/Sidebar";
 // import User from "../components/User";
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+// import AppNav from "../components/AppNav";
 
-// import styles from "./AppLayout.module.css";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>app</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
