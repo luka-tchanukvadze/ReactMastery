90f09e16ccdf24738031abfece2895823c26ba8e
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-07 21:56:36 +0400

    Programmatic Navigation with Navigate

--- 11-worldwise/learning/src/App.jsx
+++ 11-worldwise/learning/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
@@ -44,10 +44,7 @@ function App() {
           <Route path="login" element={<Login />} />
 
           <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to={"cities"} />} />
 
             <Route
               path="cities"
