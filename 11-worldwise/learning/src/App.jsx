4cd2f864deb61a5ee4282643d7b4577f2c77a6e5
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-07 16:52:00 +0400

    Programmatic Navigation with useNavigate

--- 11-worldwise/learning/src/App.jsx
+++ 11-worldwise/learning/src/App.jsx
@@ -5,8 +5,32 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = `http://localhost:8000`;
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -17,9 +41,9 @@ function App() {
           <Route path="/login" element={<Login />} />
 
           <Route path="/app" element={<AppLayout />}>
-            <Route index={<p>LIST of citiess</p>} />
+            <Route index={<CityList />} />
 
-            <Route path="cities" element={<p>something</p>} />
+            <Route path="cities" element={<CityList />} />
             <Route path="countries" element={<p>countires</p>} />
             <Route path="form" element={<p>form</p>} />
           </Route>
---
+++ 11-worldwise/learning/src/components/CityList.jsx
@@ -0,0 +1,6 @@
+import styles from "./CityList.module.css";
+
+function CityList() {
+  return <ul className={styles.CityList}>list</ul>;
+}
+export default CityList;
