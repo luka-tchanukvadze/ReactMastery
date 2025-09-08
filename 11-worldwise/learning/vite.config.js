6793cdae5293e559b4509c75da03e2b9e9fec9b9
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-08 11:34:21 +0400

    fixed vite config

--- 11-worldwise/learning/vite.config.js
+++ 11-worldwise/learning/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import eslintPlugin from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      // By default, warnings and errors are shown in overlay
+      emitWarning: true, // <-- show warnings instead of errors
+      emitError: false, // <-- don’t treat ESLint errors as Vite build errors
+      failOnError: false, // <-- don’t fail the dev server on errors
+    }),
+  ],
 });
