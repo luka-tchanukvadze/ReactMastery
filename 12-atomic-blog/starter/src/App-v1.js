887d4ae6738c3c39f04d2953a75a694470cc10e1
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-09 16:50:35 +0400

    Advanced Pattern A Custom Provider and Hook

--- 12-atomic-blog/starter/src/App-v1.js
+++ 12-atomic-blog/starter/src/App-v1.js
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
-import { PostProvider, PostContext } from "./PostContext";
 
 function createRandomPost() {
   return {
@@ -9,8 +8,33 @@ function createRandomPost() {
   };
 }
 
+// 1) CREATE A CONTEXT
+const PostContext = createContext();
+
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -22,8 +46,15 @@ function App() {
 
   return (
     // 2) PROVIDE VALUE TO CHILD COMPONENTS
-
-    <>
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
       <section>
         <button
           onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
@@ -32,14 +63,12 @@ function App() {
           {isFakeDark ? "☀️" : "🌙"}
         </button>
 
-        <PostProvider>
-          <Header />
-          <Main />
-          <Archive />
-          <Footer />
-        </PostProvider>
+        <Header />
+        <Main />
+        <Archive />
+        <Footer />
       </section>
-    </>
+    </PostContext.Provider>
   );
 }
 
