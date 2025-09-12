b8564a89f40958baf84a1c2ce10a83319dac30c1
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-12 22:17:46 +0400

    react DatePicker setup

--- 11-worldwise/learning/src/components/Form.jsx
+++ 11-worldwise/learning/src/components/Form.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
@@ -80,12 +81,13 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        {/* <input
+
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        /> */}
-        <DatePicker />
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat={"dd/MM/yyyy"}
+        />
       </div>
 
       <div className={styles.row}>
