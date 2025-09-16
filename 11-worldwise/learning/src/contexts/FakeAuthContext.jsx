cc8ef24f1b756a21a3786e0e0f682e6763144346
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-16 23:03:50 +0400

    Adding Fake Authentication Setting Up Context

--- 11-worldwise/learning/src/contexts/FakeAuthContext.jsx
+++ 11-worldwise/learning/src/contexts/FakeAuthContext.jsx
@@ -13,8 +13,17 @@ function reducer(state, action) {
       return { ...state, user: action.payload, isAuthenticated: true };
     case "logout":
       return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
   }
 }
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
 
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
@@ -22,11 +31,20 @@ function AuthProvider({ children }) {
     initialState
   );
 
-  function login(email, password) {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
 
-  function logout() {}
+  function logout() {
+    dispatch({ type: "logout" });
+  }
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
