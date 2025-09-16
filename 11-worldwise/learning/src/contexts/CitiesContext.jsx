a22aea211e1453a1ba54ba77c5c41634e71034fb
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-16 18:39:11 +0400

    fixing reducer bug

--- 11-worldwise/learning/src/contexts/CitiesContext.jsx
+++ 11-worldwise/learning/src/contexts/CitiesContext.jsx
@@ -54,7 +54,7 @@ function reducer(state, action) {
 }
 
 function CitiesProvider({ children }) {
-  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
     reducer,
     initialState
   );
