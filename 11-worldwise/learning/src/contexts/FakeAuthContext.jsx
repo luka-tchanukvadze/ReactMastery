9e78df8274431f7fe6de43ec7c0cf6284dd5d41f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-16 22:59:21 +0400

    working on reducer

--- 11-worldwise/learning/src/contexts/FakeAuthContext.jsx
+++ 11-worldwise/learning/src/contexts/FakeAuthContext.jsx
@@ -1,8 +1,20 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {}
+
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reduceRight,
+    initialState
+  );
+
   function login(email, password) {}
 
   function logout() {}
