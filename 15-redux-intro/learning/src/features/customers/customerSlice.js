f8557074b1b2cf4570b534cdf04f5b3dff45b616
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-20 16:24:06 +0400

    Professional Redux File Structure State Slices

---
+++ 15-redux-intro/learning/src/features/customers/customerSlice.js
@@ -0,0 +1,37 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "Customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "Customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "Customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "account/updateName", payload: fullName };
+}
--- 15-redux-intro/learning/src/main.jsx
+++ 15-redux-intro/learning/src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import "./store";
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 200 });
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
