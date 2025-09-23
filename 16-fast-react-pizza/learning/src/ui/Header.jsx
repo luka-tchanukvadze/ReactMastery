3774d0fbfd4bead56cbfe3dbadd712d4cba98cbb
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-23 16:09:30 +0400

    Building the App Layout

---
+++ 16-fast-react-pizza/learning/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to={"/"}>Fast React Pizza Co.</Link>
+
+      <p>Chanu</p>
+    </header>
+  );
+}
+export default Header;
