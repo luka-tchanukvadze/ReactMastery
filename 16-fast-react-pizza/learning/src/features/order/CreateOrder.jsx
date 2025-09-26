c4d80d7cc32f0543937a34c5c73a853e545df845
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-26 20:39:36 +0400

    Using the Cart for New Orders

--- 16-fast-react-pizza/learning/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/learning/src/features/order/CreateOrder.jsx
@@ -3,6 +3,9 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { clearCart, getCart } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -42,7 +45,9 @@ function CreateOrder() {
   const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
@@ -101,6 +106,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+
           <Button disabled={isSubmitting} type="primary">
             {isSubmitting ? 'Placing order....' : 'Order now'}
           </Button>
@@ -128,12 +134,12 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect
+  const newOrder = await createOrder(order);
 
-  // const newOrder = await createOrder(order);
+  // Do NOT overuse
+  store.dispatch(clearCart());
 
-  // return redirect(`/order/${newOrder.id}`);
-
-  return null;
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
