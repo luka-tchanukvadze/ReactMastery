633275dee1caf7b8082174ad545f39ec93e00daf
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-27 21:46:16 +0400

    Experiment Manual SSR With React DOM + Node.js

--- 20-manual-ssr/learning/server.js
+++ 20-manual-ssr/learning/server.js
@@ -1,7 +1,16 @@
 const { createServer } = require("http");
+const { parse } = require("url");
 
 const server = createServer((req, res) => {
-  res.end("Hello world!");
+  const pathName = parse(req.url, true).pathname;
+
+  if (pathName === "/") {
+    res.end("Hello world!");
+  } else if (pathName === "/test") {
+    res.end("Test");
+  } else {
+    res.end("The URL cannot be found");
+  }
 });
 
 server.listen(8000, () => console.log("Listening for request on port 8000"));
